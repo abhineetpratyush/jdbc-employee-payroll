58ed8a02e637af4b27d509905a66b0e1cb53badd
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-11-02 00:49:27 +0530

    [abhineet] add : ability to establish connectivity to a database from a java application

---
+++ src/main/java/com/capgemini/jdbcemployeepayroll/JDBCConnectivity.java
@@ -0,0 +1,42 @@
+package com.capgemini.jdbcemployeepayroll;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JDBCConnectivity {
+	private static final Logger log = LogManager.getLogger(JDBCConnectivity.class);
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			log.info("Driver loaded!");
+		} catch(ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath:", e);
+		}
+
+		listDrivers();
+
+		try {
+			log.info("Connecting to database: " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			log.info("Connection is successful!! " + connection);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			log.info(driverClass.getClass().getName());
+		}
+	}
+}
